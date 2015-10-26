0b4cff93a286f88c2bc39debba4c44f988316b17
phrb <pedro.bruel@gmail.com>
2015-10-26 15:04:02 -0200

    Another attempt to install Opentuner.

--- startup-script.sh
+++ startup-script.sh
@@ -1,17 +1,23 @@
 #! /bin/bash
 
+echo "[INFO] Starting up Script" > startup.log
+
 # Install Dependencies
-apt-get update
-apt-get -y install git pip
-pip install opentuner
+apt-get update &>> startup.log
+apt-get -y install git python-dev python-pip &>> startup.log
+git clone https://github.com/jansel/opentuner.git &>> startup.log
+
+cd opentuner
+pip install -r requirements.txt &>> startup.log
 
 cd /
 
 # Clone Measurement Server Repository
-git clone https://github.com/phrb/measurement-server.git
+git clone https://github.com/phrb/measurement-server.git &>> startup.log
 
+echo "[INFO] Startup Done. Starting Server" >> startup.log
 # Start Server
 cd measurement-server
-python server.py
+python server.py &>> server.log
 
 # The VM's Server is now waiting for connections and messages
