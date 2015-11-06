be2334a609b53a96804e813806396eb067bc5823
phrb <pedro.bruel@gmail.com>
2015-11-06 17:50:37 -0200

    Changes to startup script.

--- startup-script.sh
+++ startup-script.sh
@@ -4,7 +4,11 @@ echo "[INFO] Starting up Script" > startup.log
 
 cd /
 
-# Clone Measurement Server Repository
+apt-get update &>> startup.log
+apt-get install -y git python-pip python-dev python-matplotlib libsqlite3-dev libfreetype6-dev &>> startup.log
+pip install --upgrade oauth2client google-api-python-client &>> startup.log
+pip install opentuner &>> startup.log
+git clone https://github.com/jansel/opentuner.git &>> startup.log
 git clone https://github.com/phrb/measurement-server.git &>> startup.log
 
 echo "[INFO] Startup Done. Starting Server" >> startup.log
