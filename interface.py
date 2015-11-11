b4a11440ddc0f7c53a71956c9aa6a8f675ec3e09
phrb <pedro.bruel@gmail.com>
2015-11-11 21:07:33 -0200

    Try to fix error-checking in measure.

--- interface.py
+++ interface.py
@@ -107,7 +107,7 @@ class GCEInterface:
             response   = self.measure(sock, repr(config),
                                       repr(c_input), limit)
 
-            while int(response[0][1]) != NO_ERROR:
+            while response[0][1] != str(NO_ERROR):
                 self.logger.debug("Measure returned an error: {0}".format(response[0]))
                 response = self.measure(sock, repr(config),
                                         repr(c_input), limit)
